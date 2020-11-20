e9a8d7768d0b59084b1e7f45b8dfe272c654c2b6
jzolo22 <jzolotarev@gmail.com>
2020-11-19 19:44:45 -0500

    tests are not passing but the functionality is correct

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+
+// fetch("http://localhost:3000/dogs", {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     }
+//     body: JSON.stringify({
+//         dogName: "poodle", 
+//         dogBreed: "golden"
+//     })
+// })
+
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST", 
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(person => slapOn(person))
+    .catch(error => console.log(error.message))
+}
+
+const container = document.querySelector("div#container")
+
+function slapOn(person){
+    newPerson = document.createElement("p")
+    newPerson.textContent = `ID is: ${person.id}`
+    container.append(newPerson)
+}
+
+submitData("julia", "someemail")
